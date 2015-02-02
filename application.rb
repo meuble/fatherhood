3596022fecd600e00a4d81e4f984ff96a56342dc
Stéphane Akkaoui <stephane.akkaoui@sociabliz.com>
2015-02-02 01:45:32 +0100

    Add form results

--- application.rb
+++ application.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 require 'awesome_print'
 require 'haml'
 require 'yaml'
+require 'json'
 require "active_record"
 
 enable :sessions
@@ -14,6 +15,48 @@ ActiveRecord::Base.establish_connection(config["database"])
 
 class Form < ActiveRecord::Base
   validates_presence_of :data
+
+  STAI_KEYS = [
+    "good_mood", "nervous", "self_happy", "feeling_happy",
+    "feeling_failure", "feeling_rested", "cold_blooded",
+    "feeling_overwhelmed", "feeling_worried", "is_happy",
+    "disturbing_ideas", "lacking_confidence", "feeling_secured",
+    "decision_maker", "feeling_incompetent", "is_satisfied",
+    "disturbed_by_ideas", "remember_deceptions", "is_stable",
+    "is_tense"
+  ]
+
+  EDINBURG_KEYS = [
+    "has_laught", "confident_about_futur", "self_denunciation",
+    "wrongly_worried", "frightened", "mostly_overwhelmed",
+    "feeling_sad", "has_cried", "hurt_himself"
+  ]
+
+  def parsed_data
+    @parsed_data ||= JSON.parse(self.data)
+  end
+
+  def stai_result
+    result(STAI_KEYS)
+  end
+
+  def edinburg_result
+    result(EDINBURG_KEYS)
+  end
+
+  def result(keys)
+    self.parsed_data.inject(0) do |acc, v|
+      acc += v.last.to_i if keys.include?(v.first)
+      acc
+    end
+  end
+
+  def results
+    {
+      :stai => stai_result,
+      :edinburg => edinburg_result
+    }
+  end
 end
 
 get '/' do
@@ -42,10 +85,15 @@ get '/form/new' do
 end
 
 post '/forms' do
-  form = Form.new(:data => params)
+  form = Form.new(:data => params.to_json)
   if form.save
     redirect "/form/#{form.id}"
   else
     redirect "/form/new"
   end
+end
+
+get '/form/:id' do
+  form = Form.find(params[:id])
+  haml :result, :locals => {:form => form}
 end
